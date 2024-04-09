aabc007670afe51d917fd5a2dc4d531fd0377ecc
ksmnhb <162869110+ksmnhb@users.noreply.github.com>
2024-04-09 17:11:10 +0900

    kadai_mdn_js up

---
+++ kadai_mdn_js/mdn.js
@@ -0,0 +1,15 @@
+//今日を呼び出す
+var today = new Date();
+
+
+const year = today.getFullYear();
+const month = today.getMonth()+1;
+const date = today.getDate();
+
+console.log(year + "年" + month + "月" + date + "日" );
+
+
+
+
+
+
